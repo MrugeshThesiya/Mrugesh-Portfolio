a7f2684ea9178deadb81638785c994d7cba15d17
Mrugesh Thesiya <mrugeshthesiya16@gmail.com>
2024-03-23 14:09:26 +0530

    testing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import { socials } from '../constants'
 
 function Footer() {
   return (
-    <div className='w-full flex items-center justify-center flex-col'>
-    <div className='w-full flex justify-center items-center gap-[2rem]'>
+    <div className='flex items-center justify-center flex-col pb-[20px] lg:flex-row flex-wrap'>
+    <div className='flex justify-center items-center gap-[2rem]'>
       <img src={Mrugesh} alt="Image of Mrugesh Thesiya" className='w-[10rem] rounded-[50%]' />
-       <div>Contact me through my socials here : 
-       <div className="flex gap-[10px] items-center justify-center">
+       <div>
+       <p>
+         Contact me through my socials here : 
+       </p>
+       <div className="flex gap-[10px] items-center justify-center ">
             {socials.map((Element,index)=>(
                 <a key={index} className='text-[20px] hover:text-gray-400' target="_blank" href={Element.link}>{Element.title}</a>
             ))}
         </div>
        </div>
-    </div>
-    <div className='flex items-center justify-center'>
-        
     </div>
     </div>
   )
